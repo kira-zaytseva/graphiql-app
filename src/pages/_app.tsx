29e03388a61ddafe83b8adec1b74512558a4e370
Kira Zaytseva <kira.zaytseva97@gmail.com>
2023-05-09 01:04:51 +0300

    feat: add basic infrastructure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const MyApp = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>NextJS App From Scratch</title>
+      </Head>
+      <p>Hello</p>
+    </>
+  );
+};
+
+export default MyApp;

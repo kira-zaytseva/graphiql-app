25fd37a55143eebd114a614c522f1ec8063af571
Anton Lappo <anton.lappo@scorum.com>
2023-05-17 21:15:11 +0300

    feat: create button components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import './style.scss';
@@ -7,7 +6,8 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Head>
-        <title>NextJS App From Scratch</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>

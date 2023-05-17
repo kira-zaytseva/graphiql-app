9ef8441d97dce51f47882c76d8658619812e0952
Kira Zaytseva <kira.zaytseva97@gmail.com>
2023-05-18 00:39:23 +0300

    fix: move footer to index from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Head from 'next/head';
-import Footer from '../components/Footer';
+import { AppProps } from 'next/app';
 import './style.scss';
 
-const MyApp = (): JSX.Element => {
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Head>
         <title>NextJS App From Scratch</title>
       </Head>
-      <p>Hello</p>
-      <Footer />
+      <Component {...pageProps} />
     </>
   );
 };
